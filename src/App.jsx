2d02b6dcba74f8f1052d613936f872443035c42f
Harshit Shrivastava <hrshitsrivastava0@gmail.com>
2024-09-07 20:09:01 +0530

    final2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import About from "./components/About";
 export default function App() {
   return (
     <>
-      <div className="m-0 dark:bg-cyan-950 dark:text-gray-300 min-h-screen min-w-fit bg-gray-100 ">
+      <div className="m-0 dark:bg-cyan-950 dark:text-gray-300 min-h-screen min-w-fit bg-gray-100 text-black">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<SearchPage />} />
